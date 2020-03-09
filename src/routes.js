b3ab690661b4d1549897f44f3ae83c0f68830926
Bruno Souza <bruno.arubesu@gmail.com>
2020-03-08 23:26:00 -0300

    Add eslint prettier config

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
   res.json({
-    message: "Hello World"
+    message: 'Hello World',
   });
 });
 
