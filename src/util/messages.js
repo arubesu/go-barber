4e8dcb7e06d0ad6bec918743ce5f6345cc2f63a1
Bruno Souza <bruno.arubesu@gmail.com>
2020-03-22 00:07:17 -0300

    Add error messages

---
+++ src/util/messages.js
@@ -0,0 +1,9 @@
+export default {
+  error: {
+    user: {
+      alreadyExists: 'User already exists',
+      passwordInvalid: 'Password does not match',
+      notFound: 'User not found',
+    },
+  }
+}
