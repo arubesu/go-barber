ea696e60e80e2a24e3ea0d8162e08cd8a9114200
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-02 19:53:55 -0300

    Add appointments notification

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
-
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt'
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
 
@@ -98,6 +99,19 @@ class AppointmentController {
       provider_id,
       date,
     });
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM 'de' yyyy ', às ' HH:mm'h'",
+      { locale: pt }
+    )
+
+    await Notification.create({
+      //TODO: Add Internationalization
+      content: `Novo Agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id
+    })
 
     return res.json(appointment);
   }
