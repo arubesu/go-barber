79f00a9a08f65fc8907eba30d6d85b7ddd11605f
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-02 18:38:27 -0300

    add ScheduleController | Index provider schedule

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,42 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from "../models/User";
+import Appointment from "../models/Appointment";
+
+class ScheduleController {
+  async index(req, res) {
+
+    const provider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true
+      }
+    });
+
+    if (!provider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+
+    const parsedDate = parseISO(date);
+
+    const schedule = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)]
+        }
+      },
+      attributes: [
+        'id', 'date', 'user_id'
+      ]
+    })
+
+    return res.json(schedule);
+  }
+}
+
+export default new ScheduleController();
