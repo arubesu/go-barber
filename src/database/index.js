fcdbd3f47cbb860520bfeae87bb2be190006b995
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-01 23:32:23 -0300

    add file model to loader

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User'
+import File from '../app/models/File'
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -15,7 +16,6 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
-
   }
 
 }
