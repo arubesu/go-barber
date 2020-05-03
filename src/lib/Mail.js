ec6a727ddaf2b91aa1228e90d96ed30ff053c321
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-03 18:56:33 -0300

    configure templates in Mail service

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,6 +1,10 @@
 import nodemailer from 'nodemailer';
 import mailConfig from '../config/mail';
 
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
 class Mail {
   constructor() {
     const { host, port, auth, secure } = mailConfig;
@@ -11,6 +15,25 @@ class Mail {
       secure,
       auth: auth.user ? auth : null
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const emailPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use('compile', nodemailerhbs({
+      viewEngine: exphbs.create({
+        layoutsDir: resolve(emailPath, 'layouts'),
+        partialsDir: resolve(emailPath, 'partials'),
+        defaultLayout: 'default',
+        extname: '.hbs',
+      }),
+      emailPath,
+      extname: '.hbs',
+    })
+    );
+
   }
 
   sendMail(message) {
