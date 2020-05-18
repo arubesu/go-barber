742b2b7957df860b6ae6f193d36d34dbaa11788b
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-17 21:57:15 -0300

    configure helmet to add headers to secure app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
+import Helmet from 'helmet';
 
 import path from 'path';
 import * as Sentry from '@sentry/node';
@@ -23,6 +24,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(Helmet());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
