d23edd5802ebc797798c4a19f3515cdd70b52d85
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-19 00:01:20 -0300

    linting app.js

--- src/app.js
+++ src/app.js
@@ -29,25 +29,30 @@ class App {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(Helmet());
-    this.server.use(cors({
-      origin: process.env.ORIGIN_URL
-    }));
+    this.server.use(
+      cors({
+        origin: process.env.ORIGIN_URL,
+      })
+    );
 
     this.server.use(
       '/files',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
 
     if (process.env.NODE_ENV !== 'development') {
-      this.server.use(new RateLimit({
-        store: new RateLimitRedis({
-          client: redis.createClient({
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT,
-          })
-        }),
-        windowMs: 1000 * 60 * 15,
-        max: 100
-      }));
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
     }
   }
 
@@ -61,11 +66,13 @@ class App {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
 
-        return res.status(500).json(errors);
+        return res.status(err.statusCode || 500).json(errors);
       }
 
-      return res.status(500).json({ errors: 'Internal Server Error' });
-    })
+      return res
+        .status(err.statusCode || 500)
+        .json({ errors: err.message || 'Internal Server Error' });
+    });
   }
 }
 
