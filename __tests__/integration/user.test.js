b1888e1faa982c2dc04407eb813138c966d90f42
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-23 20:20:05 -0300

    Add Test:  When user tries to register using the same email

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -18,4 +18,18 @@ describe('User', () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
   });
+
+  it('Should return BadRequest when try to create an user with the same email', async () => {
+    const user = await factory.attrs('User');
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.status).toBe(400);
+  });
 });
